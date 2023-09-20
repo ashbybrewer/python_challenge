a7a6f482816d9009c3dd54f321496e22d123948d
ashbybrewer <143283503+ashbybrewer@users.noreply.github.com>
2023-09-20 12:40:23 -0500

    homework progress

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,41 @@
-import os
+import csv
 
-os.chdir(os.path.dirname(os.path.realname(__file__)))
+# open and read the CSV file
+with open("Resources/budget_data.csv", "r") as file:
+    reader = csv.reader(file)
+    
+    # storing the header row
+    header = next(reader)
+    data = list(reader)
+
+#calculating total months and total profit/loss  
+
+total_months = len(data)
+total_amount = sum(int(row[1]) for row in data)
+
+# calculating changes and then the average
+changes = [int(data[i+1][1]) - int(data[i][1]) for i in range(total_months - 1)]
+avg_change = sum(changes) / len(changes)
+
+#identifying greatest increase and decrease
+greatest_increase = max(changes)
+greatest_decrease = min(changes)
+greatest_increase_date = data[changes.index(greatest_increase) + 1][0]
+greatest_decrease_date = data[changes.index(greatest_decrease) + 1][0]
+
+
+#formatting the results 
+result= (
+    "Financial Analysis\n"
+    f"Total Months: {total_months}\n"
+    f"Total: ${total_amount}\n"
+    f"Average Change: ${avg_change:.2f}\n"
+    f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})\n"
+    f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})\n")
+
+#print results 
+print(result)
+
+#save results to txt file
+with open("analysis/financial_analysis.txt", "w") as file:
+    file.write(result)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,27 @@
-import os
+import csv
 
-os.chdir(os.path.dirname(os.path.realname(__file__)))
+#inititalize list to store csv data in 
+data= []
+
+
+#open csv file
+with open('Resources/election_data.csv', 'r') as file:
+    reader=csv.reader(file)
+    header=next(reader)# This reads the first line (header) and moves to the next line
+    for row in reader:
+        data.append(row)
+    
+#total number of votes 
+total_votes=len(data)
+
+#list of candidates and their vote counts using dictionary 
+candidates_votes = {}
+
+for row in data:
+    candidate_name = row[2]
+    if candidate_name not in candidates_votes:
+        candidates_votes[candidate_name] = 1  # If the candidate wasn't in our dictionary, add them with 1 vote
+    else:
+        candidates_votes[candidate_name] += 1  # If they were, increment their vote count
+#percentage of votes for each candidate 
+print(candidates_votes)
